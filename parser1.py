92a7d3a7e22fe1f45bac3068e4289ba5f221e249
Evhenii Prodan <alls9.pw@gmail.com>
2018-04-30 15:20:14 +0300

    optimization

--- parser1.py
+++ parser1.py
@@ -3,26 +3,32 @@ from selenium.webdriver.firefox.options import Options
 import time
 class Pars:
 	def __init__(self):
-		#city = self.get_location()
-		#categ = self.get_categ(city)
-		categ = 'https://m.avito.ru/kazan/komnaty'
-		pages = self.get_pages(categ)
-		self.get_elements(categ, pages)
+		driver = self.__driver()
+		city = self.get_location(driver)
+		categ = self.get_categ(city,driver)
+		#categ = 'https://m.avito.ru/kazan/komnaty'
+		pages = self.get_pages(categ,driver)
+		self.get_elements(categ, pages,driver)
 		print('В выбраной категории '+str(pages)+' страниц')
-	def connect_mysql:
+	def connect_mysql(self):
 		pass
 	def load_data(self):
+		pass
 	def proxy(self):
 		pass
-
-
-		ads
-	def __driver(self):
-		options = Options()
-		options.add_argument("--headless")
-		#headless браузер
-		driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
-		return driver
+	def __driver(self, headless = 1):
+		if headless == 1:
+			options = Options()
+			options.add_argument("--headless")
+			#headless браузер
+			driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+			return driver
+		if headless == 0:
+			driver = webdriver.Firefox(executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+			return driver
+		else:
+			print('Error open browser \n')
+			return 1
 	def __driver_close(self,driver):
 		driver.close()
 	def get_data(self,url,driver):
@@ -42,12 +48,8 @@ class Pars:
 			time.sleep(40)
 		#driver.close()
 		#return answer
-	def get_elements(self,url, pages):
+	def get_elements(self,url, pages, driver):
 		#метод получения ссылок объявлений на странице
-		options = Options()
-		options.add_argument("--headless")
-		#headless браузер
-		driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
 		i = 1
 		while i<=pages:
 			URL= str(url) + '?p='+str(i)
@@ -55,11 +57,7 @@ class Pars:
 			ads = driver.find_elements_by_xpath("//a[@class='item-link item-link-visited-highlight']")
 
 			self.get_data(ads,driver)
-	def get_pages(self, url):
-		options = Options()
-		options.add_argument("--headless")
-		#headless браузер
-		driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+	def get_pages(self, url,driver):
 		#получение списка регионов
 		driver.get(url)
 		try:
@@ -67,7 +65,7 @@ class Pars:
 			pages = driver.find_element_by_xpath("//div[@class='nav-helper-content nav-helper-text']")
 			pages = pages.text
 			pages = pages.replace(' ', '')
-			driver.close()
+			#driver.close()
 		
 		
 		except:
@@ -81,11 +79,7 @@ class Pars:
 		else: 
 			pages = int(pages)/20 + 1
 			return int(pages)
-	def get_location(self):
-		options = Options()
-		options.add_argument("--headless")
-		#headless браузер
-		driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+	def get_location(self, driver):
 		#получение списка регионов
 		driver.get('https://m.avito.ru/location')
 		cityes = driver.find_elements_by_xpath("//a[@class='list-link']")
@@ -100,19 +94,14 @@ class Pars:
 			return locations[location]
 		
 		else:
-			driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+			#driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
 			driver.get(location)
 			town = driver.find_elements_by_xpath("//a[@class='list-link']")
 			city =  self.__loc(town, 0)
-			driver.close()
+			#driver.close()
 			print('Вы выбрали город ' + str(city))
 			return city
-	def get_categ(self, city):
-		#функция выбора категории
-		options = Options()
-		options.add_argument("--headless")
-		#headless браузер
-		driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+	def get_categ(self, city, driver):
 		#получение списка регионов
 		driver.get(city)
 		cityes = driver.find_elements_by_xpath("//a[@class='list-link']")
@@ -124,11 +113,11 @@ class Pars:
 			return locations[location]
 		
 		else:
-			driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
+			#driver = webdriver.Firefox(firefox_options=options,executable_path=r'E:\pythonPars\geck\geckodriver.exe')
 			driver.get(location)
 			town = driver.find_elements_by_xpath("//a[@class='list-link']")
-			city =  self.__loc(town, 0)
-			driver.close()
+			city =  self.__loc(town)
+			#driver.close()
 			print('Вы выбрали категорию ' + str(city))
 			return city
 
